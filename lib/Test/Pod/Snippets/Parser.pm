5657db0393f80533fd1cac7c0d62751081390368
Yanick Champoux <yanick@babyl.dyndns.org>
2007-07-24 22:06:10 -0400

    fixed call to new()

--- lib/Test/Pod/Snippets/Parser.pm
+++ lib/Test/Pod/Snippets/Parser.pm
@@ -85,7 +85,6 @@ $DB::single = 1;
 
         if ( $parser->{tps_method_level} ) {
             if ( $paragraph =~ /^new/ ) {
-                $paragraph = '$class->'.$paragraph;
                 print {$parser->output_handle}
                     $parser->{tps}->get_object_name,
                     ' = $class->', $paragraph, ";\n";
